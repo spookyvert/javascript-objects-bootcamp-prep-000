cdf61e6e3029dbd046e73497e66c2b655dda53e4
Shemar Dacosta <dshemar6@gmail.com>
2018-11-20 17:08:06 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,11 @@ var playlist = {
   
 }
 console.log(playlist.name)
+
+function updatePlaylist(playlist,name,title){
+  return Object.assign({}, playlist, {[name]:value})
+}
+
+function removeFromPlaylist(playlist,name){
+  
+}
