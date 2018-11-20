5c84e91fb533af464a7702501a88833f6f9f5d5f
Shemar Dacosta <dshemar6@gmail.com>
2018-11-20 17:15:26 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,9 +1,16 @@
-var playlist = new Object({ artistName: 'songTitle' });
-
-function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign(playlist, { [artistName]:songTitle })
+var playlist = {
+  artistName : 'songTitle'
+  
 }
-function removeFromPlaylist(playlist, artistName) {
-  delete playlist[artistName];
+
+
+function updatePlaylist(playlist,artistName,songTitle){
+  return Object.assign({}, playlist, {[artistName]:songTitle})
+}
+
+function removeFromPlaylist(playlist,artistName){
+  delete playlist.artistName
+ 
   return playlist
+  
 }
